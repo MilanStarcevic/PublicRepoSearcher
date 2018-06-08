dd69bab58fdb5e38918663b171e9add7c5b0f4d3
Milan Starcevic <milan.starcevic@zuehlke.com>
2018-06-08 16:41:49 +0200

    Added Bitbucket search

--- PublicRepoSearch.ps1
+++ PublicRepoSearch.ps1
@@ -1,24 +1,46 @@
 param (
-    [string[]]$keywordsToSearchFor = "PublicRepoSearch",
-    [string[]]$usersToSearchFor = "MilanStarcevic"
+    [string[]]$keywordsToSearchFor = @("PublicRepoSearch", "identity", "suo"),
+    [string[]]$githubUsersToSearchFor = "MilanStarcevic",
+    [string[]]$bitbucketUsersToSearchFor = "MilanStarcevic"
 )
 
 Import-Module .\PSGithubSearch.psm1
 
-function SearchGitHub ($user) {
+function SearchGitHub () {
+    Write-Output "GitHub search"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $files = Find-GitHubCode -User $user -Keywords $keywordsToSearchFor
-    foreach ($file in $files) {
-        Write-Output "`t$($file.path)"
+
+    foreach ($user in $githubUsersToSearchFor) {
+        Write-Output "Started search for user: $($user)"
+        
+        $files = Find-GitHubCode -User $user -Keywords $keywordsToSearchFor
+        foreach ($file in $files) {
+            Write-Output "`t$($file.path)"
+        }
+    }
+}
+
+function SearchBitBucket () {
+    Write-Output "Bitbucket search"
+
+    foreach ($user in $bitbucketUsersToSearchFor) {
+        Write-Output "Started search for user: $($user)"
+
+        $searchQuery = $keywordsToSearchFor -join "+"
+        
+        $pageResponse  = Invoke-RestMethod -Uri "https://api.bitbucket.org/2.0/users/$user/search/code?search_query=$searchQuery"
+        
+        foreach ($value in $pageResponse.values) {
+            Write-Output "`t$($value.file.links.self.href)"
+        }
     }
 }
 
 function SearchAll () {  
     Write-Output "Started search for keywords: $($keywordsToSearchFor)" 
-    foreach ($user in $usersToSearchFor) {
-        Write-Output "Started search for user: $($user)"
-        SearchGitHub $user
-    }
+    
+    SearchGitHub
+    SearchBitBucket
 }
 
 SearchAll
